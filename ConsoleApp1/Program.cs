704a2b23fc9542e94503396f7cc12bf4612b3096
Feryal.Honarmand <feryhonar@gmail.com>
2025-10-29 11:21:45 +0100

    Stage 3: Add menu system, scientific operations, and history

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,61 +1,122 @@
-﻿using System;
+﻿
+namespace ConsoleApp1;
 
 class Program
 {
     static void Main()
     {
-        string? input;
+        string history1 = "";
+        string history2 = "";
+        string history3 = "";
+        string history4 = "";
+        string history5 = "";
+        string menuInput;
         do
         {
-        Console.WriteLine("Enter first number: ");
-        string firstInput = Console.ReadLine();
-        double firstNumber = double.Parse(firstInput);
-        Console.WriteLine("*********************");
+            string history = "";  
+            Console.WriteLine("=== Scientific Calculator ===");
+            Console.WriteLine("Menu:");
+            string[] menu = { "Basic Operations (+, -, *, /, %)", "Square Root", "Power (x^y)", "View History","Exit" };
+ 
+       
+            int i = 0;
+   
+            while (i < menu.Length)
+            {
+                Console.WriteLine($"{i+1}: {menu[i]}");
+                i++;
+            }
+            Console.WriteLine("Choose an option: ");
+            menuInput= Console.ReadLine();
+            switch (menuInput)
+            {
+                case "1":
+                    Console.WriteLine("Enter first number: ");
+                    string firstInput = Console.ReadLine();
+                    double firstNumber = double.Parse(firstInput);
+                    Console.WriteLine("*********************");
 
-        Console.WriteLine("Enter second number: ");
-        string secondInput = Console.ReadLine();
-        double secondNumber = double.Parse(secondInput);
-        Console.WriteLine("*********************");
+                    Console.WriteLine("Enter second number: ");
+                    string secondInput = Console.ReadLine();
+                    double secondNumber = double.Parse(secondInput);
+                    Console.WriteLine("*********************");
 
-        Console.WriteLine("Enter operation (+, -,*, /, %)");
-        string operation = Console.ReadLine();
+                    Console.WriteLine("Enter operation (+, -,*, /, %)");
+                    string operation = Console.ReadLine();
 
-        double result = 0;
+                    double result = 0;
+
+
+                    switch (operation)
+                    {
+                        case "*":
+                            result = firstNumber * secondNumber;
+                            break;
+                        case "+":
+                            result = firstNumber + secondNumber;
+                            break;
+                        case "-":
+                            result = firstNumber - secondNumber;
+                            break;
+                        case "/":
+                            if (secondNumber == 0)
+                            {
+                                Console.WriteLine("Error: Division by zero is not allowed!");
+                            }
+                            else
+                            {
+                                result = firstNumber / secondNumber;
+                            }
+                            break;
+                        case "%":
+                            result = firstNumber % secondNumber;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid operation");
+                            break;
+                    }
+                
+                    history = $"{firstNumber} {operation} {secondNumber} = {result}";
+                    Console.WriteLine("Result: " + firstNumber + operation + secondNumber + " = " + result);
+                    Console.WriteLine("*********************");
+                    break;
+                case "2":
+                    Console.WriteLine("Enter a number: ");
+                    string input = Console.ReadLine();
+                    double number = double.Parse(input);
+                    double sqrtResult =Math.Sqrt(number);
+                    history = $"{number} {input} = {sqrtResult}";
+                    Console.WriteLine($"Result: √{input} = {sqrtResult}");
+               
+                    break;
+                case "3":
+                    Console.WriteLine("Enter base: ");
+                    string baseInput = Console.ReadLine();
+                    double baseNumber=double.Parse(baseInput);
+                    Console.WriteLine("Enter exponent:");
+                    double exponentInput=double.Parse(Console.ReadLine());
+                    double powerResult=Math.Pow(baseNumber, exponentInput);
+                    history =  $"{baseNumber} {baseInput} = {powerResult}";
+                    Console.WriteLine($"Result: {baseInput}^{exponentInput} = {powerResult} ");
+                    break;
+                case "4":
+                    Console.WriteLine("=== Calculation History ===");
+                    Console.WriteLine(history1);
+                    Console.WriteLine(history2);   
+                    Console.WriteLine(history3);
+                    Console.WriteLine(history4);
+                    Console.WriteLine(history5);
+                    break;
+                case "5":
+                    Console.WriteLine("Thank you for using the Scientific Calculator!"); 
+                    break;
+            }
         
-
-    switch (operation)
-        {
-            case "*":
-                result = firstNumber * secondNumber;
-                break;
-            case "+":
-                result = firstNumber + secondNumber;
-                break;
-            case "-":
-                result = firstNumber - secondNumber;
-                break;
-            case "/":
-                if (secondNumber == 0)
-                {
-                    Console.WriteLine("Error: Division by zero is not allowed!");
-                }
-                else
-                {
-                    result = firstNumber / secondNumber;
-                }
-                break;
-            case "%":
-                result = firstNumber % secondNumber;
-                break;
-            default:
-                Console.WriteLine("Invalid operation");
-                break;
-        }
-        Console.WriteLine("Result: " + firstNumber + operation + secondNumber + " = " + result);
-        Console.WriteLine("*********************");
-        Console.WriteLine ("Do you want to do another one? (y/n)");
-        input = Console.ReadLine()?.Trim().ToLowerInvariant();
-        }while (input == "y" || input == "yes");
-        Console.WriteLine("Thank you for using the calculator!");
+            history5 = history4;
+            history4 = history3;
+            history3 = history2;
+            history2 = history1;
+            history1 = history;
+        } while (menuInput!="5");
     }
 }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8610c959d2bb24ced394686c06bd9ad53a1759b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f43a67c676c8d520ad997ee98552e3267c5fa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
